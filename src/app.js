eb20887e58c55086d65e4d53024c6eb196541012
GomezFrannco <frangomez.baires@gmail.com>
2022-06-10 20:27:27 -0300

    [Updt] Gzip compression added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const compression = require("compression");
 const dotenv = require("dotenv");
+const { notFound } = require("./controllers/404.controllers.js");
+const { getRoute } = require("./middlewares/routes.middlewares.js");
 const infoRoute = require("./routes/info.routes.js");
 const apiRoute = require("./routes/api.routes");
 
@@ -18,10 +21,13 @@ class App {
   middlewares() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(compression());
+    this.app.use(getRoute);
   }
   routes() {
     this.app.use("/", infoRoute);
     this.app.use("/", apiRoute);
+    this.app.use(notFound);
   }
   listen() {
     this.app.listen(this.app.get("port"), () => {
